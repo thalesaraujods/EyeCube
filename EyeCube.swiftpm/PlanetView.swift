49ed5448de395d4e4d5970396d0ffb50d45ff1a1
Thales Souza <t.araujods@gmail.com>
2025-02-11 14:30:33 -0400

    alterações do XCode

--- EyeCube.swiftpm/PlanetView.swift
+++ EyeCube.swiftpm/PlanetView.swift
@@ -1,10 +1,28 @@
-//
-//  Planet.swift
-//  EyeCube
-//
-//  Created by Thales Araújo on 10/02/25.
-//
-
-import Foundation
 import SwiftUI
+import SceneKit
+
+struct PlanetView: View {
+    var body: some View {
+        VStack{
+            SceneView(
+                scene: SCNScene(named: models[0].modelName),
+                options: [.autoenablesDefaultLighting, .allowsCameraControl]
+            )
+        }
+    }
+}
+
+// Data Model
+struct Model: Identifiable {
+    
+    var id: Int
+    var name: String
+    var modelName: String
+    var details: String
+}
+
+// Sample Data
+var models = [
+    Model(id: 0, name: "Pluto", modelName: "PlutoScene.scn", details: "Pluto é um planeta legal e fica no espaço."),
+]
 
